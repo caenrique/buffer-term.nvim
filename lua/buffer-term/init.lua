85126a499f310988eb7f067e3b6c7270e236fda2
Cesar Enrique Ramirez <caenrique93@gmail.com>
2022-11-17 13:47:49 +0100

    fix numbers not restoring properly and weird behaviour with terminals

--- lua/buffer-term/init.lua
+++ lua/buffer-term/init.lua
@@ -3,21 +3,31 @@ local M = {}
 local terminal = require('buffer-term.terminal')
 local config = require('buffer-term.config')
 
+local window_options = {}
+
+local function save_window_options()
+  window_options.number = vim.wo.number
+  window_options.relativenumber = vim.wo.relativenumber
+  window_options.numberwidth = vim.wo.numberwidth
+  window_options.signcolumn = vim.wo.signcolumn
+end
+
+local function restore_options()
+  for key, value in pairs(window_options) do
+    vim.wo[key] = value
+  end
+end
+
 local function configure_terminal()
-  vim.pretty_print(config)
   if config.terminal_options.no_numbers then
     vim.wo.number = false
     vim.wo.relativenumber = false
     vim.wo.numberwidth = 1
   end
-
   vim.wo.signcolumn = 'no'
-  vim.bo.modified = false
-
-  vim.bo.buflisted = config.terminal_options.buf_listed
 end
 
-local function start_insert_autocmd(buf)
+local function start_insert_autocmd(buf, group)
   vim.api.nvim_create_autocmd('BufWinEnter', {
     callback = function()
       vim.cmd('startinsert!')
@@ -29,20 +39,36 @@ local function start_insert_autocmd(buf)
 end
 
 function M.toggle(term_id)
+  -- if term_id does not exists, then create a new terminal
   terminal.last = term_id
   local term = terminal.get(term_id)
-  if term then
-    if term.visible then
+
+  if not term then
+    term = terminal.new_terminal(term_id)
+  end
+
+  local current_buf = vim.api.nvim_get_current_buf()
+  if terminal.lookup(current_buf) then -- is this buffer a terminal?
+    -- YES:
+    if current_buf == term.terminal_buffer then -- is the same as term_id?
+      -- YES: Hide it and restore window options
       terminal.hide(term_id)
+      restore_options()
     else
+      -- NO: Show it
       terminal.show(term_id)
+      configure_terminal()
     end
   else
-    terminal.new_terminal(term_id)
+    -- NO: save window options and Show it
+    save_window_options()
+    terminal.show(term_id)
+    configure_terminal()
   end
 end
 
 function M.toggle_last()
+  vim.pretty_print(terminal)
   if terminal.last then
     M.toggle(terminal.last)
   end
@@ -56,9 +82,8 @@ function M.setup(opts)
   if config.terminal_options then
     vim.api.nvim_create_autocmd('TermOpen', {
       callback = function(args)
-        configure_terminal()
         if config.terminal_options.start_insert then
-          start_insert_autocmd(args.buf)
+          start_insert_autocmd(args.buf, group)
         end
       end,
       group = group,
--- lua/buffer-term/terminal.lua
+++ lua/buffer-term/terminal.lua
@@ -1,31 +1,27 @@
 M = {}
 
+local config = require('buffer-term.config')
+
 M.instances = {}
 M.index = {}
 M.last = nil
 
-function M.hide_if_is_terminal()
-  local buffer = vim.api.nvim_win_get_buf(0)
-  local term_id = M.lookup(buffer)
-  if M.instances[term_id] then
-    M.instances[term_id].visible = false
-  end
-end
-
 function M.new_terminal(term_id)
   local origin_buffer = M.get_origin_buffer()
-
-  M.hide_if_is_terminal()
 
   vim.cmd.terminal()
 
   local buffer = vim.api.nvim_win_get_buf(0)
 
+  vim.bo[buffer].modified = false
+  vim.bo[buffer].buflisted = config.terminal_options.buf_listed
+  vim.bo[buffer].bufhidden = 'hide'
+
+  vim.api.nvim_win_set_buf(0, origin_buffer)
+
   local term = {
-    visible = true,
     origin_buffer = origin_buffer,
     terminal_buffer = buffer,
-    -- channel = channel,
   }
 
   M.instances[term_id] = term
@@ -54,7 +50,6 @@ function M.lookup(buffer)
 end
 
 function M.hide(term_id)
-  M.instances[term_id].visible = false
   local t = M.get(term_id)
 
   if t.terminal_buffer == t.origin_buffer then
@@ -65,8 +60,6 @@ function M.hide(term_id)
 end
 
 function M.show(term_id)
-  M.hide_if_is_terminal()
-  M.instances[term_id].visible = true
   M.instances[term_id].origin_buffer = M.get_origin_buffer()
   vim.api.nvim_win_set_buf(0, M.get(term_id).terminal_buffer)
 end
