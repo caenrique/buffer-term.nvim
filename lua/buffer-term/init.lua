d5f53701e4945f025d7c9f00b83fe9502f28f218
César Enrique Ramírez <caenrique93@gmail.com>
2022-11-22 17:56:20 +0100

    Feature/status line component (#6)
    * refactor: better structure for code
    
    * remove unnecesary state parameter
    
    * basic status line component

--- lua/buffer-term/init.lua
+++ lua/buffer-term/init.lua
@@ -1,79 +1,53 @@
 local M = {}
 
-local terminal = require('buffer-term.terminal')
 local config = require('buffer-term.config')
+local window = require('buffer-term.window')
 
-local window_options = {}
+local terminal = require('buffer-term.terminal')
+local state = require('buffer-term.state')
 
-local function save_window_options()
-  window_options.number = vim.wo.number
-  window_options.relativenumber = vim.wo.relativenumber
-  window_options.numberwidth = vim.wo.numberwidth
-  window_options.signcolumn = vim.wo.signcolumn
-end
-
-local function restore_options()
-  for key, value in pairs(window_options) do
-    vim.wo[key] = value
-  end
-end
-
-local function configure_terminal()
-  if config.terminal_options.no_numbers then
-    vim.wo.number = false
-    vim.wo.relativenumber = false
-    vim.wo.numberwidth = 1
-  end
-  vim.wo.signcolumn = 'no'
-end
-
-local function start_insert_autocmd(buf, group)
-  vim.api.nvim_create_autocmd('BufWinEnter', {
-    callback = function()
-      vim.cmd('startinsert!')
-    end,
-    buffer = buf,
-    group = group,
-  })
-  vim.cmd('startinsert!')
-end
-
+---Toggle the terminal buffer associated with the key `term_id`
+---@param term_id any
+---@usage `require("buffer-term").toggle('a')`
 function M.toggle(term_id)
+  if not term_id then return end
+
   -- if term_id does not exists, then create a new terminal
-  terminal.last = term_id
-  local term = terminal.get(term_id)
+  local term = state.find_by_id(term_id)
 
   if not term then
-    term = terminal.new_terminal(term_id)
+    term = terminal:new(term_id)
   end
 
+  state.last = term.id
+
   local current_buf = vim.api.nvim_get_current_buf()
-  if terminal.lookup(current_buf) then -- is this buffer a terminal?
+  if state.find_by_buffer(current_buf) then -- is this buffer a terminal?
     -- YES:
     if current_buf == term.terminal_buffer then -- is the same as term_id?
       -- YES: Hide it and restore window options
-      terminal.hide(term_id)
-      restore_options()
+      term:hide()
+      window.restore_options()
     else
       -- NO: Show it
-      terminal.show(term_id)
-      configure_terminal()
+      term:show()
+      window.configure_terminal()
     end
   else
     -- NO: save window options and Show it
-    save_window_options()
-    terminal.show(term_id)
-    configure_terminal()
+    window.save_options()
+    term:show()
+    window.configure_terminal()
   end
 end
 
+---Toggle the last terminal buffer used
+---@usage `require("buffer-term").toggle_last()`
 function M.toggle_last()
-  vim.pretty_print(terminal)
-  if terminal.last then
-    M.toggle(terminal.last)
-  end
+  M.toggle(state.last)
 end
 
+---Initialize the required configurations and hooks for the plugin
 function M.setup(opts)
   config.setup(opts)
 
@@ -83,7 +57,14 @@ function M.setup(opts)
     vim.api.nvim_create_autocmd('TermOpen', {
       callback = function(args)
         if config.terminal_options.start_insert then
-          start_insert_autocmd(args.buf, group)
+          vim.api.nvim_create_autocmd('BufWinEnter', {
+            callback = function()
+              vim.cmd('startinsert!')
+            end,
+            buffer = args.buf,
+            group = group,
+          })
+          vim.cmd('startinsert!')
         end
       end,
       group = group,
@@ -92,7 +73,7 @@ function M.setup(opts)
 
   vim.api.nvim_create_autocmd('TermClose', {
     callback = function(args)
-      terminal.delete(args.buf)
+      state.delete(args.buf)
     end,
     group = group,
   })
--- lua/buffer-term/terminal.lua
+++ lua/buffer-term/terminal.lua
@@ -1,79 +1,65 @@
-M = {}
-
 local config = require('buffer-term.config')
+local state = require('buffer-term.state')
 
-M.instances = {}
-M.index = {}
-M.last = nil
+local function get_origin_buffer()
 
-function M.new_terminal(term_id)
-  local origin_buffer = M.get_origin_buffer()
-
-  vim.cmd.terminal()
-
-  local buffer = vim.api.nvim_win_get_buf(0)
-
-  vim.bo[buffer].modified = false
-  vim.bo[buffer].buflisted = config.terminal_options.buf_listed
-  vim.bo[buffer].bufhidden = 'hide'
-
-  vim.api.nvim_win_set_buf(0, origin_buffer)
-
-  local term = {
-    origin_buffer = origin_buffer,
-    terminal_buffer = buffer,
-  }
-
-  M.instances[term_id] = term
-  M.index[buffer] = term_id
-
-  return term
-end
-
-function M.get_origin_buffer()
   local buffer = vim.api.nvim_win_get_buf(0)
   local buftype = vim.api.nvim_buf_get_option(buffer, 'buftype')
-  local term_id = M.index[buffer]
-  if buftype == 'terminal' and term_id then
-    return M.get(term_id).origin_buffer
+  local term = state.find_by_buffer(buffer)
+
+  if buftype == 'terminal' and term then
+    return term.origin_buffer
   else
     return buffer
   end
 end
 
-function M.get(term_id)
-  return M.instances[term_id]
+local Terminal = {}
+
+function Terminal:new(term_id)
+  local origin_buffer = get_origin_buffer()
+
+  vim.cmd.terminal()
+
+  local terminal_buffer = vim.api.nvim_win_get_buf(0)
+
+  vim.bo[terminal_buffer].modified = false
+  vim.bo[terminal_buffer].buflisted = config.terminal_options.buf_listed
+  vim.bo[terminal_buffer].bufhidden = 'hide'
+
+  vim.api.nvim_win_set_buf(0, origin_buffer)
+
+  local t = {
+    id = term_id,
+    origin_buffer = origin_buffer,
+    terminal_buffer = terminal_buffer,
+  }
+
+  setmetatable(t, self)
+  self.__index = self
+
+  state.set(t)
+
+  return t
 end
 
-function M.lookup(buffer)
-  return M.index[buffer]
-end
-
-function M.hide(term_id)
-  local t = M.get(term_id)
-
-  if t.terminal_buffer == t.origin_buffer then
+function Terminal:hide()
+  if self.terminal_buffer == self.origin_buffer or self.origin_buffer == nil then
     local empty_buf = vim.api.nvim_create_buf(true, false)
-    M.instances[term_id].origin_buffer = empty_buf
+    self.origin_buffer = empty_buf
+    state.set(self)
   end
-  vim.api.nvim_win_set_buf(0, M.get(term_id).origin_buffer)
+  vim.api.nvim_win_set_buf(0, self.origin_buffer)
 end
 
-function M.show(term_id)
-  M.instances[term_id].origin_buffer = M.get_origin_buffer()
-  vim.api.nvim_win_set_buf(0, M.get(term_id).terminal_buffer)
+function Terminal:show()
+  self.origin_buffer = get_origin_buffer()
+  state.set(self)
+  vim.api.nvim_win_set_buf(0, self.terminal_buffer)
 end
 
-function M.delete(buffer)
-  local buftype = vim.api.nvim_buf_get_option(buffer, 'buftype')
-  local term_id = M.lookup(buffer)
-  if buftype == 'terminal' and term_id then
-    M.instances[term_id] = nil
-    M.index[buffer] = nil
-    if M.last == term_id then
-      M.last = nil
-    end
-  end
+function Terminal:delete()
+  state.delete(self.terminal_buffer)
 end
 
-return M
+return Terminal
